de112c6be48fb82dc749bad8105b3217b706eeee
Fabitheguy003 <fabienmukungu@gmail.com>
2023-03-23 19:14:28 -0400

    index.js for Header and Navigation done

--- Develop/src/components/About/index.js
+++ Develop/src/components/About/index.js
@@ -19,10 +19,10 @@ function About() {
                     Hi I'm Fabien Mac - As a full-stack student, I am passionate about learning and developing both front-end and back-end technologies. My journey started with the basic concepts of web development, such as HTML, CSS, and JavaScript. However, as I progressed, I realized the importance of a holistic understanding of web development, which led me to learn server-side languages such as Node.js, Express.js, React and MongoDB, among others.
                 </p>
                 <p>
-                In my work, I strive to create dynamic, responsive web applications that add value to the end user. In my work, I focus on efficient, and user-friendly applications. I enjoy exploring new technologies and frameworks to stay up-to-date with the latest trends and to continuously improve my skills.
+                    In my work, I strive to create dynamic, responsive web applications that add value to the end user. In my work, I focus on efficient, and user-friendly applications. I enjoy exploring new technologies and frameworks to stay up-to-date with the latest trends and to continuously improve my skills.
                 </p>
                 <p>
-                I believe that effective communication and collaboration are key to achieving success in software development. I am always eager to work with other developers, designers, and stakeholders to create innovative solutions and to learn from the expertise of others. As a full-stack student, I am constantly learning and growing, and I am excited about the endless possibilities of web development.
+                    I believe that effective communication and collaboration are key to achieving success in software development. I am always eager to work with other developers, designers, and stakeholders to create innovative solutions and to learn from the expertise of others. As a full-stack student, I am constantly learning and growing, and I am excited about the endless possibilities of web development.
                 </p>
             </div>
         </section>
---
+++ Develop/src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function Header(props) {
+    const { currentTab, setCurrentTab } = props;
+
+    return (
+        <header>
+            <div className="name">
+                <h2>Fabien Mac</h2>
+            </div>
+            <div>
+                <Navigation
+                    currentTab={currentTab}
+                    setCurrentTab={setCurrentTab}
+                ></Navigation>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
